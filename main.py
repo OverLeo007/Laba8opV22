97e3feb4554e508b80535020cd61a18f989e1d37
Paskal <Vaxxo9000@gmail.com>
2021-12-27 18:36:13 +0700

    added entry point

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from delivery.terminal import Terminal
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Terminal()
+    ex.show()
+    app.exec_()
+    exit()
